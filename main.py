21816d17bd032fd05b0681073cdbea73a9c02792
Ivan Shlykov <ivan.shlykov.materials@mail.ru>
2026-01-30 19:14:09 +0300

    add function for main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Think of god')
+from dotenv import load_dotenv
+import os
+
+def print_author():
+    load_dotenv()  # загрузит переменные из .env (если он рядом с main.py)
+    author = os.getenv("AUTHOR")
+    print(f"Автор проекта: {author}")
+
+print_author()
